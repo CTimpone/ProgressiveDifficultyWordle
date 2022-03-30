8cb1ef7e3ff6887dba77e4451a8e2d43587f2016
CTimpone <ctimponegit@gmail.com>
2022-03-30 11:50:27 -0400

    Configure Grunt

--- ProgressiveDiffiultyWordle/wwwroot/js/Session - Copy.js
+++
@@ -1,7 +0,0 @@
-var PDW;
-(function (PDW) {
-    class SessionState {
-    }
-    PDW.SessionState = SessionState;
-})(PDW || (PDW = {}));
-//# sourceMappingURL=Session%20-%20Copy.js.map
--- ProgressiveDiffiultyWordle/wwwroot/js/SingleGame - Copy.js
+++
@@ -1,7 +0,0 @@
-var PDW;
-(function (PDW) {
-    class Session {
-    }
-    PDW.Session = Session;
-})(PDW || (PDW = {}));
-//# sourceMappingURL=SingleGame%20-%20Copy.js.map
