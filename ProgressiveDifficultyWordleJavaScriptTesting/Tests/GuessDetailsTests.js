b5241d6a17ec0a4ff61e049a65a1d0ad13d7c2ab
CTimpone <ctimponegit@gmail.com>
2022-04-05 17:16:36 -0400

    Additional GuessDetails tests

--- ProgressiveDifficultyWordleJavaScriptTesting/Tests/GuessDetailsTests.js
+++ ProgressiveDifficultyWordleJavaScriptTesting/Tests/GuessDetailsTests.js
@@ -4,6 +4,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const assert = require("assert");
 const sinon = require("sinon");
 const GuessDetails_1 = require("../../progressivedifficultywordle/typescript/models/GuessDetails");
+const letterstatus_1 = require("../../progressivedifficultywordle/typescript/models/letterstatus");
 describe("GuessDetails", () => {
     var consoleSpy;
     beforeEach(() => {
@@ -50,6 +51,90 @@ describe("GuessDetails", () => {
             assert.equal(2, map.get('a').length);
             assert.equal(0, map.get('a')[0]);
             assert.equal(2, map.get('a')[1]);
+        });
+    });
+    describe("#populateCharacterStates", () => {
+        it('fills character states array entirely with ExactMatch status when input maps are identical.', () => {
+            let gd = new GuessDetails_1.GuessDetails('alert', 'alert');
+            gd.characterStates = new Array(5);
+            let guessMap = new Map();
+            let answerMap = new Map();
+            guessMap.set('a', [0]);
+            answerMap.set('a', [0]);
+            guessMap.set('l', [1]);
+            answerMap.set('l', [1]);
+            guessMap.set('e', [2]);
+            answerMap.set('e', [2]);
+            guessMap.set('r', [3]);
+            answerMap.set('r', [3]);
+            guessMap.set('t', [4]);
+            answerMap.set('t', [4]);
+            gd.populateCharacterStates(guessMap, answerMap);
+            for (let i = 0; i < 5; i++) {
+                assert.equal(letterstatus_1.LetterStatus.ExactMatch, gd.characterStates[i]);
+            }
+        });
+        it('fills character states array entirely with Absent status when input maps have no intersection.', () => {
+            let gd = new GuessDetails_1.GuessDetails('wrong', 'table');
+            gd.characterStates = new Array(5);
+            let guessMap = new Map();
+            let answerMap = new Map();
+            guessMap.set('w', [0]);
+            answerMap.set('t', [0]);
+            guessMap.set('r', [1]);
+            answerMap.set('a', [1]);
+            guessMap.set('o', [2]);
+            answerMap.set('b', [2]);
+            guessMap.set('n', [3]);
+            answerMap.set('l', [3]);
+            guessMap.set('g', [4]);
+            answerMap.set('e', [4]);
+            gd.populateCharacterStates(guessMap, answerMap);
+            for (let i = 0; i < 5; i++) {
+                assert.equal(letterstatus_1.LetterStatus.Absent, gd.characterStates[i]);
+            }
+        });
+        it('fills character states array entirely with WrongLocation status when input maps have no intersection.', () => {
+            let gd = new GuessDetails_1.GuessDetails('later', 'alert');
+            gd.characterStates = new Array(5);
+            let guessMap = new Map();
+            let answerMap = new Map();
+            guessMap.set('l', [0]);
+            answerMap.set('a', [0]);
+            guessMap.set('a', [1]);
+            answerMap.set('l', [1]);
+            guessMap.set('t', [2]);
+            answerMap.set('e', [2]);
+            guessMap.set('e', [3]);
+            answerMap.set('r', [3]);
+            guessMap.set('r', [4]);
+            answerMap.set('t', [4]);
+            gd.populateCharacterStates(guessMap, answerMap);
+            for (let i = 0; i < 5; i++) {
+                assert.equal(letterstatus_1.LetterStatus.WrongLocation, gd.characterStates[i]);
+            }
+        });
+        it('fills character states array entirely with all statuses to accurately represent intersection.', () => {
+            let gd = new GuessDetails_1.GuessDetails('risen', 'paver');
+            gd.characterStates = new Array(5);
+            let guessMap = new Map();
+            let answerMap = new Map();
+            guessMap.set('r', [0]);
+            answerMap.set('p', [0]);
+            guessMap.set('i', [1]);
+            answerMap.set('a', [1]);
+            guessMap.set('s', [2]);
+            answerMap.set('v', [2]);
+            guessMap.set('e', [3]);
+            answerMap.set('e', [3]);
+            guessMap.set('n', [4]);
+            answerMap.set('r', [4]);
+            gd.populateCharacterStates(guessMap, answerMap);
+            assert.equal(letterstatus_1.LetterStatus.WrongLocation, gd.characterStates[0]);
+            assert.equal(letterstatus_1.LetterStatus.Absent, gd.characterStates[1]);
+            assert.equal(letterstatus_1.LetterStatus.Absent, gd.characterStates[2]);
+            assert.equal(letterstatus_1.LetterStatus.ExactMatch, gd.characterStates[3]);
+            assert.equal(letterstatus_1.LetterStatus.Absent, gd.characterStates[4]);
         });
     });
 });
