cc0ed0f51c711525c1c822e16e442ae71b27e85a
CTimpone <ctimponegit@gmail.com>
2022-04-18 17:11:15 -0400

    Overhaul linting to not be terrible

--- ProgressiveDifficultyWordle/Gruntfile.js
+++ ProgressiveDifficultyWordle/Gruntfile.js
@@ -12,6 +12,15 @@ module.exports = function (grunt) {
                 src: ["temp/"],
             }
         },
+        eslint: {
+            options: {
+                overrideConfigFile: 'eslint.json',
+            },
+            target: [
+                "TypeScript/**/*.ts",
+                "TypeScript/**/**/*.ts"
+            ]
+        },
         concat: {
             scripts: {
                 src: ['wwwroot/js/GameLogic/*.js', 'wwwroot/js/Models/*.js'],
@@ -20,13 +29,6 @@ module.exports = function (grunt) {
             words: {
                 src: ['wwwroot/js/Constants/Words/*.js'],
                 dest: 'temp/words.js'
-            }
-        },
-        jshint: {
-            files: ['temp/*.js'],
-            options: {
-                'esversion': 6,
-                '-W069': false,
             }
         },
         uglify: {
@@ -60,12 +62,12 @@ module.exports = function (grunt) {
     });
 
     grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks("grunt-eslint");
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask("code", ['clean:codeOnly', 'concat:scripts', 'jshint', 'uglify:codeMin', 'clean:tempOnly']);
-    grunt.registerTask("all", ['clean:complete', 'concat:scripts', 'concat:words', 'jshint', 'uglify:codeMin', 'uglify:wordsMin', 'clean:tempOnly']);
+    grunt.registerTask("code", ['clean:codeOnly', 'concat:scripts', 'eslint', 'uglify:codeMin', 'clean:tempOnly']);
+    grunt.registerTask("all", ['clean:complete', 'concat:scripts', 'concat:words', 'eslint', 'uglify:codeMin', 'uglify:wordsMin', 'clean:tempOnly']);
 
 };
