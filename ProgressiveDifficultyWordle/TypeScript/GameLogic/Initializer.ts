6f8c5ea438bea9a97d09f58d0cc7941616d84c1d
CTimpone <ctimponegit@gmail.com>
2022-04-18 18:25:19 -0400

    Overhaul packing with WebPack

--- ProgressiveDifficultyWordle/TypeScript/GameLogic/Initializer.ts
+++ ProgressiveDifficultyWordle/TypeScript/GameLogic/Initializer.ts
@@ -1,3 +1,21 @@
-﻿$(document).ready(function () {
-    console.log("Not yet implemented.")
+﻿import { GameType } from '../Models/GameType';
+import { LetterStatus } from '../Models/LetterStatus';
+import { NotificationEventing } from '../Models/Notification/NotificationEventing';
+import { NotificationWrapper } from '../Models/Notification/NotificationWrapper';
+import { Session } from '../Models/Session';
+
+$(document).ready(function () {
+    const notifications = new NotificationEventing();
+    const notifyFn = (notification: NotificationWrapper) => {
+        console.log("Not yet implemented.");
+    };
+    notifications.registerListener(notifyFn);
+
+    const paintFn = (words: string[], letterStatuses: LetterStatus[][]) => {
+        console.log("Not yet implemented.");
+    };
+    const session = new Session(GameType.Single, false, notifications, paintFn);
+
+    console.log("Not yet implemented.");
+
 });
---
+++ ProgressiveDifficultyWordle/webpack.config.js
@@ -0,0 +1,22 @@
+﻿const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './TypeScript/GameLogic/Initializer.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'scriptbundle.js',
+        path: path.resolve(__dirname, 'wwwroot/lib/'),
+    },
+};
