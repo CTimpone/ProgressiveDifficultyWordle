b44b60403af8e736c4f93570c24a843662cdb9f9
CTimpone <ctimponegit@gmail.com>
2022-04-19 15:20:34 -0400

    More tests

--- ProgressiveDifficultyWordle/wwwroot/js/Models/ScoreDetails.js
+++ ProgressiveDifficultyWordle/wwwroot/js/Models/ScoreDetails.js
@@ -30,8 +30,9 @@ class ScoreDetails {
             const lastGuess = game.userGuesses[guessCount - 1];
             roundScore = lastGuess.fullMatch ? 1000 : 0;
             roundScore += 500 * ScoreDetails.ROUND_SCORE_GUESS_MULTIPLIERS[guessCount - 1];
-            const timeElapsedMinutes = Math.floor((game.endTime.getTime() - game.startTime.getTime()) / 60000);
-            if (timeElapsedMinutes <= 4) {
+            let timeElapsedMinutes = Math.ceil((game.endTime.getTime() - game.startTime.getTime()) / 60000);
+            timeElapsedMinutes = timeElapsedMinutes === 0 ? 1 : timeElapsedMinutes;
+            if (timeElapsedMinutes <= 5) {
                 roundScore = (roundScore * 2) / Math.sqrt(timeElapsedMinutes);
             }
             if (this.startingGuesses.has(game.userGuesses[0].guess)) {
@@ -40,13 +41,10 @@ class ScoreDetails {
             else {
                 this.startingGuesses.add(game.userGuesses[0].guess);
             }
-            this.totalScore += roundScore;
-            this.roundsCompleted += 1;
-            this.totalScore = this.totalScore * (Math.log(100 + this.roundsCompleted) / Math.log(100));
         }
         return roundScore;
     }
 }
 exports.ScoreDetails = ScoreDetails;
 ScoreDetails.ROUND_SCORE_GUESS_MULTIPLIERS = [2, 1.5, 1.25, 1, 0.8, 0.5];
-//# sourceMappingURL=scoredetails.js.map
+//# sourceMappingURL=ScoreDetails.js.map
