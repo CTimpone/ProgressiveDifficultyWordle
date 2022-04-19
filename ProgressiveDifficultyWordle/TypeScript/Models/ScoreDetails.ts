06f2d48b784b3c93b56e8a198aadcf18be358307
CTimpone <ctimponegit@gmail.com>
2022-04-19 15:20:34 -0400

    More tests

--- ProgressiveDifficultyWordle/TypeScript/Models/ScoreDetails.ts
+++ ProgressiveDifficultyWordle/TypeScript/Models/ScoreDetails.ts
@@ -42,9 +42,10 @@ export class ScoreDetails {
 
             roundScore += 500 * ScoreDetails.ROUND_SCORE_GUESS_MULTIPLIERS[guessCount - 1];
 
-            const timeElapsedMinutes = Math.floor((game.endTime.getTime() - game.startTime.getTime()) / 60000);
+            let timeElapsedMinutes = Math.ceil((game.endTime.getTime() - game.startTime.getTime()) / 60000);
+            timeElapsedMinutes = timeElapsedMinutes === 0 ? 1 : timeElapsedMinutes;
 
-            if (timeElapsedMinutes <= 4) {
+            if (timeElapsedMinutes <= 5) {
                 roundScore = (roundScore * 2) / Math.sqrt(timeElapsedMinutes);
             }
 
@@ -53,10 +54,6 @@ export class ScoreDetails {
             } else {
                 this.startingGuesses.add(game.userGuesses[0].guess);
             }
-
-            this.totalScore += roundScore;
-            this.roundsCompleted += 1;
-            this.totalScore = this.totalScore * (Math.log(100 + this.roundsCompleted) / Math.log(100));
         } 
 
         return roundScore;
